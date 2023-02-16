b9d68d5255194ce00515fb016303c8b963f06496
neeraj-gogate <neeraj.gogate@student.allenisd.org>
2023-02-15 23:34:19 -0600

    Updated System for Parsing Through Input

--- Back End/main.cpp
+++ Back End/main.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include <string>
 #include <vector>
-void print_vector(std::vector<std::vector<std::string> > vec){
+void print_vector(std::vector<std::string> vec){
     for (int i = 0; i < vec.size(); i++) {
-        for (int j = 0; j < vec[i].size(); j++) {
-            std::cout << vec[i][j] << std::endl;
-        }
+            std::cout << vec[i] << std::endl;
     }
 }
 std::vector<std::string> takeInput() {
-    std::vector<std::string> value;
+    std::vector<std::string>  value;
     for (std::string line; std::getline(std::cin, line);){
         value.push_back(line);
     }
     return value;
 }
-std::vector<std::vector<std::string> > split_spaces(std::vector<std::string> raw){
-    std::vector<std::vector<std::string> > split;
+std::vector<std::string>  remove_spaces(std::vector<std::string>  raw){
+    std::vector<std::string> newOne;
     for (int i = 0; i < raw.size(); i++){
-        std::string str = raw[i];
-        split.resize(i+1);
-        std::string delimiter = " ";
-        int start = 0;
-        int end = str.find(" ");
-        while (end != -1) {
-            split[i].push_back(str.substr(start, end - start));
-            start = end + delimiter.size();
-            end = str.find(delimiter, start);
+        std::string emp;
+        for (int j = 0; j < raw[i].length(); j++){
+            if (!isspace(raw[i][j])){
+                emp = emp + raw[i][j];
+            } 
         }
-        split[i].push_back(str.substr(start, end - start));
+        newOne.push_back(emp);
     }
-    return split;
+    return newOne;
 }
+
+
 int main() {
-    std::vector<std::string> initial = takeInput();
-    std::vector<std::vector<std::string> > split = split_spaces(initial);
-    print_vector(split);
+    std::vector<std::string> lines = remove_spaces(takeInput());
+    print_vector(lines);
+    //std::vector<Variable> variables;
     return 0;
 }
