2f60be06ef3c1e28c27f0b3586d6fa2436d14ab2
VivekMaranganti <vivek.maranganti@gmail.com>
2023-02-15 21:44:02 -0600

    Added basic if-loop conversion and commit messages for print conversions function.

--- Back End/MockInputConversions.cpp
+++ Back End/MockInputConversions.cpp
@@ -3,16 +3,17 @@
 #include <vector>
 #include <sstream>
 
-
+// Creating a Mock Input
 std::vector<std::vector<std::string> > mockInput
 {
     {"print(\"quotesTest\")"},
     {"print(noQuotesTest)"},
     {"print(multipleOnOneLineTest)", "print(multipleOnOneLineTest)"},
-    {"not a print test"}
+    {"not a print test"},
+    {"if testVar1 = testVar2 {"}
 };
 
-
+// Function to print the indexes of a 2d array
 void printVector(std::vector<std::vector<std::string> > testArr) {
     for (int i = 0; i < testArr.size(); i++)
     {
@@ -25,23 +26,28 @@ void printVector(std::vector<std::vector<std::string> > testArr) {
 
 };
 
-class Conversions {
+class printStatements {
+// Class dedicated to the conversion of print statements from swift to c++
 
     public:
-     std::vector<std::vector<std::string> > printConversion(std::vector<std::vector<std::string> > testArr) {
+     std::vector<std::vector<std::string> > Conversion(std::vector<std::vector<std::string> > testArr) {
         for (int i = 0; i < testArr.size(); i++) {
 
             for (int j = 0; j < testArr[i].size(); j++) {
+                // Looks for "print(" in each index of the array to determine weather there is a print statement or not.
                 size_t found = testArr[i][j].find("print()", 0, 6);
 
                 if (found != std::string::npos) {
+                    // The next 3 lines work to create a substring, containing all of the text inside the parinthesis of a print statement.
                     int requiredLength = 6;
                     int endLength = testArr[i][j].length() - requiredLength;
                     std::string printContent = testArr[i][j].substr(requiredLength, endLength - 1);
 
+                    // Creates a string with the syntax of a C++ print statement, along with the content inside the parinthesis of the original print statment.
                     std::ostringstream varInStr;
                     varInStr << "std::cout <<" << " " << printContent << ";";
 
+                    // Causes the current index to contain the new, converted C++ code.
                     testArr[i][j] = varInStr.str();
                     
                 }
@@ -57,9 +63,52 @@ class Conversions {
 
 };
 
-Conversions conversions = Conversions();
+class Loops {
+
+    public:
+        std::vector<std::vector<std::string> > ifConversion(std::vector<std::vector<std::string> > testArr) {
+            for (int i = 0; i < testArr.size(); i++) {
+
+                for (int j = 0; j < testArr[i].size(); j++) {
+                    std::string myStr = testArr[i][j];
+                    std::string startStr = myStr.substr(0, 2);
+                    size_t found = startStr.find("if");
+
+                    if (found != std::string::npos) {
+                        int startLength = 3;
+                        int endLength = testArr[i][j].length() - startLength;
+                        std::string ifContent = testArr[i][j].substr(startLength, endLength - 1);
+
+                        std::ostringstream ifFormat;
+                        ifFormat << "if" << " " << "(" << ifContent << ")" << " " << "{";
+
+                        testArr[i][j] = ifFormat.str();
+                    } else {
+                        testArr[i][j] = testArr[i][j];
+                    };
+                };
+            };
+            return testArr;
+        };
+
+
+        std::vector<std::vector<std::string> > forConversion(std::vector<std::vector<std::string> > testArr) {
+            return testArr;
+        };
+
+
+        std::vector<std::vector<std::string> > whileConversion(std::vector<std::vector<std::string> > testArr) {
+            return testArr;
+        };
+};
+
+
+
+printStatements print = printStatements();
+Loops loops = Loops();
 
 int main() {
-    std::vector<std::vector<std::string> > printCheck = conversions.printConversion(mockInput);
-    printVector(printCheck);
+    std::vector<std::vector<std::string> > printCheck = print.Conversion(mockInput);
+    std::vector<std::vector<std::string> > ifCheck = loops.ifConversion(printCheck);
+    printVector(ifCheck);
 };
