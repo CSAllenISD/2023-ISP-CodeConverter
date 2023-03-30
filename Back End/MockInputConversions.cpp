1fea1e7227c3212877b06205510bfeca1b6b9888
VivekMaranganti <vivek.maranganti@gmail.com>
2023-03-30 18:57:10 -0500

    added while loop conversion

--- Back End/MockInputConversions.cpp
+++ Back End/MockInputConversions.cpp
@@ -10,7 +10,8 @@ std::vector<std::vector<std::string> > mockInput
     {"print(noQuotesTest)"},
     {"print(multipleOnOneLineTest)", "print(multipleOnOneLineTest)"},
     {"not a print test"},
-    {"if testVar1 = testVar2 {"}
+    {"if testVar1 = testVar2 {"},
+    {"while testVar1 = testVar2 {"}
 };
 
 // Function to print the indexes of a 2d array
@@ -66,45 +67,71 @@ class printStatements {
 class Loops {
 // Class dedicated to the conversion of print statements from swift to c++
 
-    public:
-        std::vector<std::vector<std::string> > ifConversion(std::vector<std::vector<std::string> > testArr) {
-            for (int i = 0; i < testArr.size(); i++) {
+public:
+  std::vector<std::vector<std::string> > ifConversion(std::vector<std::vector<std::string> > testArr) {
+    for (int i = 0; i < testArr.size(); i++) {
 
-                for (int j = 0; j < testArr[i].size(); j++) {
+      for (int j = 0; j < testArr[i].size(); j++) {
 
-                    // Looks for "if" in each index of the array to determine weather there is a print statement or not.
-                    std::string myStr = testArr[i][j];
-                    std::string startStr = myStr.substr(0, 2);
-                    size_t found = startStr.find("if");
+	// Looks for "if" in each index of the array to determine weather there is a print statement or not.
+	std::string myStr = testArr[i][j];
+	std::string startStr = myStr.substr(0, 2);
+	size_t found = startStr.find("if");
 
-                    if (found != std::string::npos) {
-                        // The next 3 lines work to create a substring, containing all of the text after the word "if".
-                        int startLength = 3;
-                        int endLength = testArr[i][j].length() - startLength;
-                        std::string ifContent = testArr[i][j].substr(startLength, endLength - 1);
+	if (found != std::string::npos) {
+	  // The next 3 lines work to create a substring, containing all of the text after the word "if".
+	  int startLength = 3;
+	  int endLength = testArr[i][j].length() - startLength;
+	  std::string ifContent = testArr[i][j].substr(startLength, endLength - 1);
 
-                        std::ostringstream ifFormat;
-                        ifFormat << "if" << " " << "(" << ifContent << ")" << " " << "{";
+	  std::ostringstream ifFormat;
+	  ifFormat << "if" << " " << "(" << ifContent << ")" << " " << "{";
 
-                        // Causes the current index to contain the new, converted C++ code.
-                        testArr[i][j] = ifFormat.str();
-                    } else {
-                        testArr[i][j] = testArr[i][j];
-                    };
-                };
-            };
-            return testArr;
-        };
-
-
-        std::vector<std::vector<std::string> > forConversion(std::vector<std::vector<std::string> > testArr) {
-            return testArr;
-        };
+	  // Causes the current index to contain the new, converted C++ code.
+	  testArr[i][j] = ifFormat.str();
+	} else {
+	  testArr[i][j] = testArr[i][j];
+	};
+      };
+    };
+    return testArr;
+  };
 
 
-        std::vector<std::vector<std::string> > whileConversion(std::vector<std::vector<std::string> > testArr) {
-            return testArr;
-        };
+  std::vector<std::vector<std::string> > forConversion(std::vector<std::vector<std::string> > testArr) {
+    return testArr;
+  };
+
+
+  std::vector<std::vector<std::string> > whileConversion(std::vector<std::vector<std::string> > testArr) {
+    for (int i = 0; i < testArr.size(); i++) {
+
+      for (int j = 0; j < testArr[i].size(); j++) {
+
+	// Looks for "for" in each index of the array to determine weather there is a for statement or not.
+	std::string myStr = testArr[i][j];
+	std::string startStr = myStr.substr(0, 5);
+	size_t found = startStr.find("while");
+
+	if (found != std::string::npos) {
+	  // The next 3 lines work to create a substring, containing all of the text after the word "if".
+	  int startLength = 6;
+	  int endLength = testArr[i][j].length() - startLength;
+	  std::string whileContent = testArr[i][j].substr(startLength, endLength - 1);
+
+	  std::ostringstream whileFormat;
+	  whileFormat << "while" << " " << "(" << whileContent << ")" << " " << "{";
+
+	  // Causes the current index to contain the new, converted C++ code.
+	  testArr[i][j] = whileFormat.str();
+	} else {
+	  testArr[i][j] = testArr[i][j];
+	};
+      };
+    };
+	  
+    return testArr;
+  };
 };
 
 
@@ -116,7 +143,8 @@ printStatements print = printStatements();
 Loops loops = Loops();
 
 int main() {
-    std::vector<std::vector<std::string> > printCheck = print.Conversion(mockInput);
-    std::vector<std::vector<std::string> > ifCheck = loops.ifConversion(printCheck);
-    printVector(ifCheck);
+  std::vector<std::vector<std::string> > printCheck = print.Conversion(mockInput);
+  std::vector<std::vector<std::string> > ifCheck = loops.ifConversion(printCheck);
+  std::vector<std::vector<std::string> > whileCheck = loops.whileConversion(ifCheck);
+  printVector(whileCheck);
 };
