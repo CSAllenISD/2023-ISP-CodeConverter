af4ce1a3e00933970d93f3ef4348a1cc56159da2
neeraj-gogate <neeraj.gogate@student.allenisd.org>
2023-04-01 03:16:33 -0500

    Bug Fixes
    Bug Fixing with the HTML file and minor bugs with overlap between variable names and function names

--- Back End/Value.cpp
+++ Back End/Value.cpp
@@ -9,34 +9,34 @@ Value::Value(){
     int n;
 }
 std::vector<std::string> Value::convert(std::string line, std::map<std::string, std::string> vars_n){
-    std::vector<std::string> var;
-    var.push_back(line);
-    var.push_back("unknown");
-    
+    std::vector<std::string> l;
+    l.push_back(line);
+    l.push_back("unknown");
     auto it{vars_n.cbegin()};
     for (auto const& [key, val] : vars_n){
         if (line.rfind(key,0) == 0){
             std::string swiftVarFunc = line.substr(key.size(),line.size());
             std::vector<std::string> newFunc = varFunctions(swiftVarFunc, val);
             std::string newL = key + newFunc[0];
-            newFunc[0] = newL;
-            return newFunc;
+            l[0] = newL;
+            l[1] = newFunc[1];
+            return l;
         }
     }
-    return var;
+    return l;
 }
 
 std::vector<std::string> Value::varFunctions(std::string func, std::string type){
     //inneficient and sloppy as hell but I will fix later, I'm too tired
-    std::vector<std::string> var;
-    var.push_back(func);
-    var.push_back("TYPE NOT FOUND");
+    std::vector<std::string> l;
+    l.push_back(func);
+    l.push_back("TYPE NOT FOUND");
     if (type == "std::string"){
         if (func == ".count"){
-            var[0] = ".size()";
-            var[1] = "double";
-            return var;
+            l[0] = ".size()";
+            l[1] = "int";
+            return l;
         }
     }
-    return var;
+    return l;
 }
