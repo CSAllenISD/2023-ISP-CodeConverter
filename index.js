5d69bf6cbb9518bce1fca381a56ad6907e2f3a33
Karhik Peravali <you@example.com>
2023-04-01 01:33:31 -0500

    Created FAQ page, edited CSS and JS

--- index.js
+++ index.js
@@ -35,7 +35,25 @@ function fileUpload() {
 	button.addEventListener('click', function() {
 	    hiddenInput.click();
 	});
-    };
+    });
 
 			       
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+	    var openDropdown = dropdowns[i];
+	    if (openDropdown.classList.contains('show')) {
+		openDropdown.classList.remove('show');
+	    }
+	}
+    }
 }
